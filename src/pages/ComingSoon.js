603bf6cf8199ea18d35949cdeacfef4d6cfec253
Dom <domnguyen@gmail.com>
2024-09-06 20:04:03 -0700

    Add missing "Coming Soon" page component

---
+++ src/pages/ComingSoon.js
@@ -0,0 +1,10 @@
+function ComingSoon() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Coming Soon</h1>
+      <p>Stay tuned! This page is under construction and will be available soon.</p>
+    </div>
+  );
+}
+
+export default ComingSoon;
